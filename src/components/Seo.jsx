ba2a23ce368ffd7eb1d6ec74c3125b6e75ed6cd2
Coco K <coco@mac.mynetworksettings.com>
2025-06-11 11:40:46 -0400

    added canonical urls to every page for SEO purposes

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import { useLocation } from "@reach/router"
 
 export default function Seo({
   title,
@@ -10,7 +11,7 @@ export default function Seo({
   canonical,
   noindex,
   nofollow,
-  children, 
+  children,
 }) {
   const { site } = useStaticQuery(graphql`
     query {
@@ -25,61 +26,59 @@ export default function Seo({
     }
   `)
 
+  const { pathname } = useLocation()
 
-  const defaultTitle = site.siteMetadata?.title // "Juno Realty Partners"
-  const seoDescroption = description || site.siteMetadata.description
-  const ogMetaDescriptionText = ogMetaDescription || description
- 
   const siteUrl = site.siteMetadata.siteUrl.replace(/\/$/, "")
 
+  const canonicalUrl = canonical
+    ? canonical.startsWith("http")
+      ? canonical
+      : `${siteUrl}${canonical}`
+    : `${siteUrl}${pathname}`
 
-  // const metaImage = image
-  //   ? `${siteUrl}${image.images.fallback.src}`
-  //   : null
-      const metaImage = image
-    ? image.images.fallback.src
-    : null
+  const defaultTitle = site.siteMetadata?.title // "Juno Realty Partners"
+  const seoDescription = description || site.siteMetadata.description
+  const ogMetaDescriptionText = ogMetaDescription || description
+
+  const metaImage = image ? image.images.fallback.src : null
 
   return (
     <Helmet>
       {/* Primary tags */}
-      <title>
-        {defaultTitle ? `${title} | ${defaultTitle}` : title}
-      </title>
-      <meta name="description" content={seoDescroption} />
+      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <meta name="description" content={seoDescription} />
 
-      {/* Canonical URL */}
-      {canonical && (
-        <link
-          rel="canonical"
-          href={canonical.startsWith("http")
-            ? canonical
-            : `${siteUrl}${canonical}`}
-        />
-      )}
+      {/* Canonical Tag: always non-www.siteUrl + path */}
+      <link rel="canonical" href={canonicalUrl} />
 
       {/* Robots */}
       {(noindex || nofollow) && (
         <meta
           name="robots"
-          content={`${noindex ? "noindex" : "index"},${nofollow ? "nofollow" : "follow"}`}
+          content={`${noindex ? "noindex" : "index"},${
+            nofollow ? "nofollow" : "follow"
+          }`}
         />
       )}
 
       {/* Open Graph */}
       <meta property="og:type" content="website" />
-      <meta property="og:title" content= {defaultTitle ? `${title} | ${defaultTitle}` : title} />
+      <meta
+        property="og:title"
+        content={defaultTitle ? `${title} | ${defaultTitle}` : title}
+      />
       <meta property="og:description" content={ogMetaDescriptionText} />
-      {metaImage && (
-          <meta property="og:image" content={metaImage} />
-      )}
+      {metaImage && <meta property="og:image" content={metaImage} />}
 
       {metaImage && (
-              <meta property="og:image:alt" content={image.description || title} />
+        <meta property="og:image:alt" content={image.description || title} />
       )}
 
       {/* Twitter */}
-      <meta name="twitter:card" content={metaImage ? "summary_large_image" : "summary"} />
+      <meta
+        name="twitter:card"
+        content={metaImage ? "summary_large_image" : "summary"}
+      />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={ogMetaDescriptionText} />
       {metaImage && <meta name="twitter:image" content={metaImage} />}
